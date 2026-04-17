23a199e215d9324e5b5a8e5c17b87a9d0ed5e84a
Maximo Benjamin Rodriguez <maximobenjaminrodriguez01@gmail.com>
2026-04-17 11:11:39 -0300

    Se añadio la ventana para agregar los vehiculos

--- src/main/java/app/Program.java
+++ src/main/java/app/Program.java
@@ -1,13 +1,21 @@
 package app;
+import data.Persistencia;
+import views.AgregarVehiculos;
+import javax.swing.*;
+import java.awt.*;
 
 import data.Persistencia;
 import java.util.InvalidPropertiesFormatException;
-import views.ListarVehiculosView;
 
 public class Program {
     public static void main(String[] args) throws IllegalArgumentException, InvalidPropertiesFormatException {
-        Persistencia.inicializar();
-        ListarVehiculosView view = new ListarVehiculosView();
-        view.setVisible(true);
+     Persistencia.inicializar();
+        
+        // 2. Iniciamos directamente la ventana de Agregar Vehículo
+        java.awt.EventQueue.invokeLater(() -> {
+            AgregarVehiculos ventanaIngreso = new AgregarVehiculos();
+            ventanaIngreso.setLocationRelativeTo(null); // Centra la ventana en la pantalla
+            ventanaIngreso.setVisible(true);
+        });
     }
 }
--- src/main/java/domain/VehiculoCombustible.java
+++ src/main/java/domain/VehiculoCombustible.java
@@ -1,18 +1,17 @@
 package domain;
-
-import java.time.LocalDate;
+import java.time.Year;
 
 public class VehiculoCombustible extends Vehiculo {
     private double kilometrosPorLitro;
     private double litrosExtra;
 
-    public VehiculoCombustible(String patente, String marca, String modelo, int anio, double capacidadCarga,
+    public VehiculoCombustible(String patente, Marca marca, String modelo, int anio, double capacidadCarga,
                                Sucursal sucursal, double kilometrosPorLitro, double litrosExtra) {
         super(VehiculoTipo.COMBUSTIBLE, patente, marca, modelo, anio, capacidadCarga, sucursal);
         this.kilometrosPorLitro = kilometrosPorLitro;
         this.litrosExtra = litrosExtra;
     }
-    
+
      public double getKilometrosPorLitro() {
         return kilometrosPorLitro;
     }
@@ -23,8 +22,15 @@ public class VehiculoCombustible extends Vehiculo {
 
     @Override
     public double calcularConsumo(double kilometros) {
-        double extras = LocalDate.now().getYear() - anio > 5 ? (kilometros / 15) * litrosExtra : 0;
-        double total = (kilometros / kilometrosPorLitro) + extras;
-        return total;
+
+        double totalLitros = kilometros / kilometrosPorLitro;
+
+        int antiguedad = Year.now().getValue() - this.anio;
+
+        if(antiguedad > 5){
+            totalLitros += litrosExtra;
+        }
+
+        return totalLitros;
     }
 }
